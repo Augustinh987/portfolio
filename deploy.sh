2788e058c1706c3bbe20a73e8dc10e0f65ba6933
augustinh987 <augustin.hussenet@gmail.com>
2022-08-08 20:01:59 +0200

    First commit

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,9 @@ npm run build
 
 cd dist
 
+git init
 git add -A
 git commit -m "New Deployment"
-git push -f https://github.com/Augustinh987/portfolio.git main:gh-pages
+git push -f git@github.com:Augustinh987/portfolio.git master:gh-pages
 
 cd -
